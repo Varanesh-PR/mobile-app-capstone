873b724ab794840f2d95dbcd0ce113ba12ce2668
Varanesh-PR <166776902+Varanesh-PR@users.noreply.github.com>
2026-03-10 18:55:07 +0530

    Create LoginScreen.js

---
+++ LoginScreen.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+function LoginScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    if (!email || !password) {
+      alert("Please enter email and password");
+    } else {
+      alert("Login successful");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>Login</button>
+
+      <p>Don't have an account? Sign Up</p>
+    </div>
+  );
+}
+
+export default LoginScreen;
