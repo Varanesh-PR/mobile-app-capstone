cd5ae68828a34032cf99f49d2c593f031c1dffdb
Varanesh-PR <166776902+Varanesh-PR@users.noreply.github.com>
2026-03-10 19:38:25 +0530

    Create SettingsMenu.js

---
+++ SettingsMenu.js
@@ -0,0 +1,13 @@
+function SettingsMenu(){
+ return(
+  <div>
+   <h2>Settings Menu</h2>
+   <ul>
+    <li>Profile</li>
+    <li>Notifications</li>
+    <li>Logout</li>
+   </ul>
+  </div>
+ )
+}
+export default SettingsMenu;
