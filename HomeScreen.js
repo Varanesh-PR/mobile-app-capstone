f5370c769ff82ad0a6437278fedfc966db0da1fe
Varanesh-PR <166776902+Varanesh-PR@users.noreply.github.com>
2026-03-10 19:06:14 +0530

    Create HomeScreen.js

---
+++ HomeScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function HomeScreen() {
+  return (
+    <div>
+      <h1>Home Screen</h1>
+
+      <p>Welcome to the Mobile App</p>
+
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+
+    </div>
+  );
+}
+
+export default HomeScreen;
